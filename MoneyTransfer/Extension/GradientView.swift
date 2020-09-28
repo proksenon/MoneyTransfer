560b7b86af9cd31da163f932beb18e69c10e743c
Nikita Zabludovskiy <NMZabludovskiy@sberbank.ru>
2020-09-28 14:39:14 +0300

    fixed gradient

--- MoneyTransfer/Extension/GradientView.swift
+++ MoneyTransfer/Extension/GradientView.swift
@@ -18,7 +18,7 @@ extension UIView {
 	func gradient(with colors: [CGColor], startPoint: CGPoint = CGPoint(x: 0, y: 0), stopPoint: CGPoint = CGPoint(x: 1, y: 1), frame: CGRect? = nil){
 
 		let gradientLayer = CAGradientLayer()
-		gradientLayer.colors = [UIColor.gray.cgColor, UIColor.black.cgColor]//Colors you want to add
+		gradientLayer.colors = colors
 		gradientLayer.startPoint = startPoint
 		gradientLayer.endPoint = stopPoint
 		if let frame = frame {
